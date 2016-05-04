65f5c03a598069f82290834ff7ccf4885e1d6147
crystalfire7 <12specialreindeers@gmail.com>
2016-05-04 00:14:22 -0500

    Adding new methods and comments into Unit and implementing UIElement

--- src/UIElement.java
+++ src/UIElement.java
@@ -1,6 +1,36 @@
+//Java imports
+import java.util.List;
+
+//JavaFx Imports
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Shape;
 
 public abstract class UIElement extends ImageView{
+	//Shape of UIElement used to check for intersection
+	private Shape collisionShape;
 	
+	/**Abstract method for the creation of collision shape in each subclass
+	 */
+	abstract void createCollShape();
+
+	/**Retrieves the collision shape for the element
+	 * @return collision shape of the element used to check for intersections
+	 */
+	public Shape getCollShape()
+	{
+		return collisionShape;
+	}
+	
+	/**Checks the current shape with another given shape and checks for intersection. This method uses the intersect method in Shape,
+	 * which returns a shape representing the intersection of the two shapes given. It then converts the intersection shape into a path. 
+	 * If there are more than zero elements in the path then there is an intersection.
+	 * @param other other UI element which is checked for intersection
+	 * @return whether the two shapes intersect
+	 */
+	public boolean checkCollision(UIElement other)
+	{
+		return ((Path)Shape.intersect(this.collisionShape, other.getCollShape())).getElements().size() > 0;
+	}
 	
 }
--- src/Unit.java
+++ src/Unit.java
@@ -3,75 +3,95 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Unit extends UIElement{
-	private int health, attackRange, damage, mvmt, cost;
+	
+	//Instance variables for the unit to store its statistics
+	private int health;
+	private double attackRange;
+	private int damage;
+	private double moveRange;
+	private int cost;
 	private ImageView appearance;
 	
-	/**
-	 * @param health
-	 * @param attackRange
-	 * @param damage
-	 * @param mvmt
-	 * @param cost
+	/**Creates a unit with the given statistics of health, attack/movement ranges, damage, cost, and appearance
+	 * @param health the health value that the unit has
+	 * @param attackRange the distance that a bullet fired by this unit can travel
+	 * @param damage damage that a bullet fired by this unit will do to an enemy unit
+	 * @param moveRange the distance that this unit can travel in one turn
+	 * @param cost the amount of money that it takes for a Player to purchase this unit
+	 * @param appearance the image that this unit will display on the UI screen
 	 */
-	public Unit (int health, int attackRange, int damage, int mvmt, int cost)
+	public Unit (int health, double attackRange, int damage, double moveRange, int cost, ImageView appearance)
 	{
 		this.health = health;
 		this.attackRange = attackRange;
 		this.damage = damage;
-		this.mvmt = mvmt;
+		this.moveRange = moveRange;
 		this.cost = cost;
-		//will create in Tools method based on type of unit
-		appearance = null;
+		this.appearance = appearance;
 	}
-	/**
-	 * 
-	 * @return health of the unit
+	
+	/**Returns the health of the unit
+	 * @return the health value that the unit has
 	 */
 	public int getHealth()
 	{
 		return health;
 	}
-	/**
-	 * 
-	 * @return attack range of the unit
+	
+	/**Returns the attack range of the unit
+	 * @return the distance that a bullet fired by this unit can travel
 	 */
-	public int getAttackRange()
+	public double getAttackRange()
 	{
 		return attackRange;
 	}
-	/**
-	 * 
-	 * @return damage of the unit
+	
+	/**Returns the damage of the unit
+	 * @return damage that a bullet fired by this unit will do to the health of an enemy unit
 	 */
 	public int getDamage()
 	{
 		return damage;
 	}
-	/**
-	 * 
-	 * @return movement range of the unit
+	
+	/**Returns the movement range of the unit
+	 * @return the distance that this unit can travel in one turn
 	 */
-	public int getMovementRange()
+	public double getMovementRange()
 	{
-		return mvmt;
+		return moveRange;
 	}
-	/**
-	 * 
-	 * @return cost of the unit
+	
+	/**Returns the cost of the unit
+	 * @return the amount of money that it takes for a Player to purchase this unit
 	 */
 	public int getCost()
 	{
 		return cost;
 	}
 	
+	/**Returns the appearance of this unit in an ImageView
+	 * @return the image that this unit will display on the UI screen
+	 */
 	public ImageView getAppearance()
 	{
 		return appearance;
 	}
-	public void setAppearance(Image img, double height, double width, double xLoc, double yLoc,
-			 double widthRatio, double heightRatio, double smallestRatio, Effect eff)
+	
+	/**Sets the new health of the unit
+	 * @param the health value that the unit has
+	 */
+	public void setHealth(int newHealth)
 	{
-		appearance = Tools.createImageView(img, height, width, xLoc, yLoc, widthRatio, heightRatio, smallestRatio, eff);
+		health = newHealth;
+	}
+	
+	/**Sets the appearance of this unit
+	 * @param ImageView that the unit will be changed to
+	 */
+	public void setAppearance(ImageView picture)
+	{
+		appearance = picture;
 	}
 	
 }
