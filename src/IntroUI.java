502c0c40d91d39ada0476659eba74382bcb36939
Shishir V. Jessu <shishir.jessu@gmail.com>
2016-05-12 11:13:28 -0500

    Projectile Fix

--- src/IntroUI.java
+++ src/IntroUI.java
@@ -1,12 +1,6 @@
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class IntroUI extends Application{
+public class IntroUI extends Stage{
 	
 
 }
--- src/Projectile.java
+++ src/Projectile.java
@@ -1,30 +1,14 @@
 
 public class Projectile extends UIElement
-{	
-	private double startX;
-	private double startY;
-	private double endX;
-	private double endY;
+{
 	private double damage;
 	
-	/**Creates a projectile that starts from a location and has an ending location.
-	 * @param startX
-	 * @param startY
-	 * @param endX
-	 * @param endY
-	 * @param damage
+	/**Creates a projectile that has a certain damage
+	 * @param damage the amount of damage the bullet will inflict to a unit if it hits it
 	 */
-	public Projectile(double startX, double startY, double endX, double endY, double damage) 
+	public Projectile(double damage) 
 	{
-		this.startX = startX;
-		this.startY = startY;
-		this.endX = endX;
-		this.endY = endY;
 		this.damage = damage;
 	}
-	
-	double xVelocity, yVelocity;
-	int damage, range;
-	 
 	
 }
